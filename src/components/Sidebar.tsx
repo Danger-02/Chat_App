817ad0faec9ffb54009b22c7e941a9ca81fab2f2
Prabhas Chaubey <prabhaschaubey46@gmail.com>
2025-04-13 20:15:02 +0530

    new components added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,6 +6,8 @@ import React from 'react'
 import { Button } from './ui/button';
 import { cn } from '@/lib/utils';
 import { LogOut } from 'lucide-react';
+import useSound from 'use-sound';
+import { usePreferences } from '@/store/usePreferences';
 
 interface SidebarProps {
     isCollapsed: boolean;
@@ -13,6 +15,8 @@ interface SidebarProps {
 
 const Sidebar = ({isCollapsed}: SidebarProps) => {
     const selectedUser = USERS[0];
+	const [playClickSound]=useSound("/sounds/mouse-click.mp3")
+	const { soundEnabled }=usePreferences()
 
   return (
     <div className='relative flex flex-col h-full gap-4 p-2 data-[collapsed=true]:p-2 max-h-full overflow-auto bg-background'>
@@ -30,7 +34,10 @@ const Sidebar = ({isCollapsed}: SidebarProps) => {
 						<TooltipProvider key={idx}>
 							<Tooltip delayDuration={0}>
 								<TooltipTrigger asChild>
-                                    <div>
+									
+                                    <div onClick={()=> {
+										soundEnabled && playClickSound()
+									}}>
 										<Avatar className='my-1 flex justify-center items-center'>
 											<AvatarImage
 												src={user.image || "/user-placeholder.png"}
@@ -59,6 +66,9 @@ const Sidebar = ({isCollapsed}: SidebarProps) => {
 								selectedUser?.email === user.email &&
 									"dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white shrink"
 							)}
+							onClick={()=> {
+								soundEnabled && playClickSound()
+							}}
 						>
 							<Avatar className='flex justify-center items-center'>
 								<AvatarImage
